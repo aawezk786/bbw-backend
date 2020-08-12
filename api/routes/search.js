ccca331dbd399038fceff26a841c8aacff8e9dd4
aawezk456 <aawezk456@gmail.com>
2020-08-12 16:04:56 +0530

    update search api

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
-// const algoliasearch = require('algoliasearch');
-// const client = algoliasearch('JF5EPDPH1R','dfa3ef2bde335a6e8d006df5eabdde15');
-// const index = client.initIndex('bbw');
 
 router.get('/',async (req,res,next)=>{
   if(req.query.book_name == '1'){
-    const searchKeyword = req.query.searchKeyword
+    let searchKeyword = req.query.searchKeyword
     ? {
         book_name: {
           $regex: req.query.searchKeyword,
@@ -15,7 +12,7 @@ router.get('/',async (req,res,next)=>{
         },
       }
     : {};
-    const products = await Book.find({ ...searchKeyword });
+    let products = await Book.find({ ...searchKeyword });
    return res.status(200).json({
       success: true,
       count : products.length +" " + "Results found",
@@ -23,7 +20,7 @@ router.get('/',async (req,res,next)=>{
     });
   }
   if(req.query.author_name == '1'){
-    const searchKeyword = req.query.searchKeyword
+    let searchKeyword = req.query.searchKeyword
     ? {
         author_name: {
           $regex: req.query.searchKeyword,
@@ -31,7 +28,7 @@ router.get('/',async (req,res,next)=>{
         },
       }
     : {};
-    const products = await Book.find({ ...searchKeyword });
+    let products = await Book.find({ ...searchKeyword });
    return res.status(200).json({
       success: true,
       count : products.length +" " + "Results found",
@@ -42,18 +39,6 @@ router.get('/',async (req,res,next)=>{
     message : "Check query"
   });
     
-    
-    // var regex = new RegExp(req.params.name.toLowerCase(),'i');
-    // Book.find({book_name: regex})
-    // .then(result=>{
-    //     res.status(200).json(result);
-    // })
-    // .catch(err=>{
-    //     res.status(404).json({
-    //         message : "Not Found",
-    //         error : err
-    //     });
-    // });
 });
 
 
