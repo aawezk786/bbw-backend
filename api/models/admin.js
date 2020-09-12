afd537239eaa15c2df3c7a4bcbe6c93a8c590209
aawezk456 <aawezk456@gmail.com>
2020-09-12 14:34:34 +0530

    Update: Admin

--- api/models/admin.js
+++ api/models/admin.js
@@ -4,7 +4,7 @@ const adminSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {type : String},
     phonenumber : {type : Number,unique : true},
-    email   :{type : String ,unique : true},
+    email   :{type : String },
     password : {type : String},
     role : {type:String}
 
