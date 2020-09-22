8f1a992ddab35094093439383829f0c1038da5dd
aawezk456 <aawezk456@gmail.com>
2020-09-22 19:12:36 +0530

    Update : Update Subcat to Category

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -6,12 +6,12 @@ const mongoose = require('mongoose');
 
 exports.create_cats = (req, res, next) => {
     const category = new Category({
-        _id : new mongoose.Types.ObjectId(),
-        icon_name : req.body.icon_name,
-        category : req.body.category,
-        subcategory : req.body.subcategory
+        _id: new mongoose.Types.ObjectId(),
+        icon_name: req.body.icon_name,
+        category: req.body.category,
+        subcategory: req.body.subcategory
     });
-        category.save()
+    category.save()
         .then(category => {
             return res.status(201).json({
                 message: 'Category was created',
@@ -24,38 +24,38 @@ exports.create_cats = (req, res, next) => {
 }
 
 
-exports.getall_cats = (req,res,next)=>{
-    Category.find().sort({category : 1}).populate('subcategory')
-    .exec()
-    .then(docs =>{
-        if(docs.length >=0){
-            res.status(200).json(docs);
-        }else{
-            res.status(404).json({
-                message : "No Category Found"
-            });
-        }
-    })
-    .catch(err =>{
-        next(err);
-    });
-}
-
-exports.deleteCats = (req,res,next)=>{
-    Category.deleteOne({_id : req.params.catId})
-    .exec()
-    .then(docs =>{
-        res.status(200).json({
-            message : "Category was Deleted"
+exports.getall_cats = (req, res, next) => {
+    Category.find().sort({ category: 1 }).populate('subcategory')
+        .exec()
+        .then(docs => {
+            if (docs.length >= 0) {
+                res.status(200).json(docs);
+            } else {
+                res.status(404).json({
+                    message: "No Category Found"
+                });
+            }
         })
-    })
-    .catch(err =>{
-        next(err);
-    });
+        .catch(err => {
+            next(err);
+        });
+}
+
+exports.deleteCats = (req, res, next) => {
+    Category.deleteOne({ _id: req.params.catId })
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                message: "Category was Deleted"
+            })
+        })
+        .catch(err => {
+            next(err);
+        });
 }
 
 
-exports.deleteSubcats =  (req, res, next) => {
+exports.deleteSubcats = (req, res, next) => {
     const subcatId = req.query.subcatId;
     const catId = req.params.catId
     Category.findOneAndUpdate({ _id: catId }, { $pull: { "subcategory": { _id: subcatId } } }).exec()
@@ -65,7 +65,24 @@ exports.deleteSubcats =  (req, res, next) => {
                     message: "Subcategory Is Deleted From Category"
                 });
             }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+}
 
+exports.UpdateSubcat = (req, res, next) => {
+    const subcatId = req.query.subcatId;
+    const catId = req.params.catId
+    Category.findOneAndUpdate({ _id: catId }, { $push: { "subcategory": { _id: subcatId } } }).exec()
+        .then(result => {
+            if (result) {
+                res.status(200).json({
+                    message: "Subcategory Added To Category"
+                });
+            }
         })
         .catch(err => {
             res.status(500).json({
--- api/routes/category.js
+++ api/routes/category.js
@@ -6,5 +6,6 @@ router.post('/', CatControllers.create_cats );
 router.get('/', CatControllers.getall_cats);
 router.delete('/:catId', CatControllers.deleteCats);
 router.delete('/:catId', CatControllers.deleteSubcats);
+router.post('/:catId', CatControllers.UpdateSubcat);
 
 module.exports = router;
